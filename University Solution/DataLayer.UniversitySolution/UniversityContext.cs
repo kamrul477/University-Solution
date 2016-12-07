57db7126dce5cf88956820c1c88c8266b7c9493e
kamrul <kamrul@KAMRUL-PC>
2016-12-07 22:35:55 +0600

    7th commit

---
+++ University Solution/DataLayer.UniversitySolution/UniversityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.UniversitySolution;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DataLayer.UniversitySolution
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Degree> Degrees { get; set; }
+        public DbSet<DegreeRequirment> DegreeRequirments { get; set; }
+    }
+}
