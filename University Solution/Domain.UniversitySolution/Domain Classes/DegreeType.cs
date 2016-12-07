a8f9e29c275fb56889d482fa80398398bad5ed02
kamrul <kamrul@KAMRUL-PC>
2016-12-07 21:22:26 +0600

    5th commit

--- University Solution/Domain.UniversitySolution/Domain Classes/DegreeType.cs
+++ University Solution/Domain.UniversitySolution/Domain Classes/DegreeType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace Domain.UniversitySolution
 {
     public class DegreeType
     {
+        [Key]
+        public Guid DegreeTypeId { get; set; }
+
+        [Display(Name="Type of Degree")]
+        public string DegreeTypeTitle { get; set; }
+
+
+        //navigational properties
+        public virtual ICollection<Degree> Degrees { get; set; }
+
     }
 }
